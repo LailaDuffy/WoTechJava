ce00f5defc9c1345b17af9cc88d7143bd9097b45
Laila <78215618+LailaDuffy@users.noreply.github.com>
2024-05-30 13:16:56 +0300

    Create Lesson1.java

---
+++ Lesson1.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    var name = "Laila";
+    System.out.println("name");
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}

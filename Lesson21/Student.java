f6c539da46dd4e36a4ecad3b04bea1b26f616e53
Laila <78215618+LailaDuffy@users.noreply.github.com>
2024-06-01 14:14:25 +0300

    Create Student.java

---
+++ Lesson21/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+
+  // static class variable, default = 0
+  public static int studentCount; // global variable that everyone can use
+  
+  public String name;
+  public int age;
+  
+}

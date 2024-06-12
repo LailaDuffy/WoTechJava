d01d29543a561b384bf377c1aa1ecaaefdc9565f
Laila <78215618+LailaDuffy@users.noreply.github.com>
2024-06-12 15:30:26 +0300

    Create main.java

---
+++ Lesson22/main.java
@@ -0,0 +1,4 @@
+/*main.java -> we are getting the user input, we are sending this input to taskManager, show information to the user
+taskManager.java -> we can add tasks to the list, we can mark them as done and we can get the list
+task.java -> template for a task (name, description, date, itIsDone)*/
+

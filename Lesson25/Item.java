ead5ac748833c1f92f514fa799cc1eb7cdb08928
Laila <78215618+LailaDuffy@users.noreply.github.com>
2024-06-13 20:53:02 +0300

    Create Item.java

---
+++ Lesson25/Item.java
@@ -0,0 +1,36 @@
+public class Item {
+  private String name;
+  private int cost; // payment by gold pieces
+  private int id;
+
+  // constructor that takes care of baseline logic 
+  public Item(int id, String name, int cost) {
+    this.id = id;
+    this.name = name;
+    this.cost = cost;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  // setter for name to control what happens when the name is changed
+  public void setName(String name) {
+    // do some logic here like add timestamp
+    this.name = name;
+    // do some other logic like a notification that the name has been changed
+  }
+
+  public void setCost(int cost) {
+    this.cost = cost;
+  }
+
+  // getter for name
+  public String getName() {
+    return name;
+  }
+
+  public int getCost() {
+    return cost;
+  }  
+}

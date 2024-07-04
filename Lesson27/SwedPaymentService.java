20f343440e959300e0768356c2746ee06c18afff
Laila <78215618+LailaDuffy@users.noreply.github.com>
2024-07-04 20:40:18 +0300

    Create SwedPaymentService.java

---
+++ Lesson27/SwedPaymentService.java
@@ -0,0 +1,7 @@
+public class SwedPaymentService extends PaymentService {
+
+    @Override
+    public void sendPayment() {
+        System.out.println("This is a payment to Swedbank service");
+    }
+}

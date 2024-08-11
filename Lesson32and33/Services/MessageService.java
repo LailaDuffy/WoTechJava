4446f761a68f477bbc71c2347865381d1a7e4cd7
Laila <78215618+LailaDuffy@users.noreply.github.com>
2024-08-11 16:57:58 +0300

    Create MessageService.java

---
+++ Lesson32and33/Services/MessageService.java
@@ -0,0 +1,10 @@
+package com.datorium.Datorium.API.Services;
+
+import com.datorium.Datorium.API.DTOs.Message;
+
+public class MessageService {
+
+    public String sendText(Message message) {
+        return "Hello Datorium";
+    }
+}

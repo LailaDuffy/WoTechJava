47a53b44529198b5d43b89fadcdc5a0ce6fd71d8
Laila <78215618+LailaDuffy@users.noreply.github.com>
2024-08-11 16:58:16 +0300

    Create UserService.java

---
+++ Lesson32and33/Services/UserService.java
@@ -0,0 +1,10 @@
+package com.datorium.Datorium.API.Services;
+
+import com.datorium.Datorium.API.DTOs.User;
+
+public class UserService {
+
+    public int add(User user){
+        return 0;
+    }
+}
